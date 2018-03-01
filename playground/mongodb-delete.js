4914665b2d168d0aa81d7f13d225aa743a5e3721
sailokesh533 <sailokesh533@gmail.com>
2018-03-01 23:30:17 +0530

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unable to  Connect');
+  }
+  console.log('Successfully connected');
+// db.collection('Users').deleteMany({
+//   'name':'Jyothi'
+// }).then((result)=>{
+//   console.log(result);
+// })
+// db.collection('Users').deleteOne({
+//   _id:new ObjectID('5a95a14b1e246332c0038ff8')
+// }).then((result)=>{
+//   console.log(result);
+// });
+
+db.collection('Users').findOneAndDelete({
+  'name':'Kinnu'
+}).then((result)=>{
+  console.log(JSON.stringify(result,undefined,2));
+});
+  db.close();
+});
